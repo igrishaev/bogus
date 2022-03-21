5e8f38cf4ef19fad5d57888b619fb7a1c73ce279
Ivan Grishaev <ivan@grishaev.me>
2022-03-21 15:39:15 +0300

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.igrishaev/bogus "0.1.1-SNAPSHOT"
+(defproject com.github.igrishaev/bogus "0.1.1"
 
   :description
   "A small, GUI-powered, NIH-reasoned debugger for Clojure"
